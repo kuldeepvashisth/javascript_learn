df1116ed46ff677630c63b8b7d0e5a17ae8b709b
kuldeepvashisth <vashisthdeep829@gmail.com>
2025-12-22 18:16:05 +0000

    basics

---
+++ basics/fundamentals.js
@@ -0,0 +1,54 @@
+
+/*
+ let str="  abcd";
+console.log(str.toUpperCase());
+console.log(str.trim());
+
+let str1="20";
+let str2="25"
+console.log(str1+str2) //--> 2025
+console.log (Number(str1)+Number(str2)); //-->45<--
+console.log (+str1 + +str2); //-->45<--    
+
+let age=23;
+let name="kd";
+ let aboutme= `my name is ${name} and age is ${age}`;
+console.log(aboutme) ;
+
+//  comparator 
+// == -> compare valuevise
+// === -> compare value & datatype vise
+// != -> regular or !== -> check data type also
+//
+let num1=7,num2="7";
+ 
+console.log(num1>=num2)
+console.log(num1 === num2)
+console.log(num1 != num2)
+console.log(num1 !== num2)
+ falsy;- "" , null,undefined,0 
+ insb pr falsy man jayega to jo falsy wali condition hogi vo execute hogi 
+
+ truthy:- "abc"(kuch value dedi),1,-1{basivcaly kuch bhi agr hai}
+
+if(num1%2==0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+ 
+//terminary operator -> if else in 1 line --> jaise 1 line return 
+let age=23;
+ let drink= age > 12 ?" milk" : "Coffee";
+ console.log(drink);
+
+ */
+ //logical AND/OR operator same as cpp;
+ let name="kuldeep",age=34;
+ if(name[2]=== 'l' || age>=22){
+    console.log("adult");
+ }
+ else{
+    Minor;
+} 
