8dbaa5c89deac255a21056b5ace63fd32f2e0128
kuldeepvashisth <vashisthdeep829@gmail.com>
2025-12-23 08:05:27 +0000

    basics

---
+++ basics/f.js
@@ -0,0 +1,78 @@
+
+/*
+ let str="  abcd";
+console.log(str.toUpperCase());
+console.log(str.trim());
+
+let str1="20";
+let str2="25"
+console.log(str1+str2) //--> 2025
+console.log (Number(str1)+Number(str2)); //-->45<--
+console.log (+str1 + +str2); //-->45<--    
+
+let age=23;
+let name="kd";
+ let aboutme= `my name is ${name} and age is ${age}`;
+console.log(aboutme) ;
+
+//  comparator 
+// == -> compare valuevise
+// === -> compare value & datatype vise
+// != -> regular or !== -> check data type also
+//
+let num1=7,num2="7";
+ 
+console.log(num1>=num2)
+console.log(num1 === num2)
+console.log(num1 != num2)
+console.log(num1 !== num2)
+ falsy;- "" , null,undefined,0 
+ insb pr falsy man jayega to jo falsy wali condition hogi vo execute hogi 
+
+ truthy:- "abc"(kuch value dedi),1,-1{basivcaly kuch bhi agr hai}
+
+if(num1%2==0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+ 
+"all loops and if else all are same as c++
+
+"//terminary operator -> if else in 1 line --> jaise 1 line return 
+let age=23;
+ let drink= age > 12 ?" milk" : "Coffee";
+ console.log(drink);
+
+ //logical AND/OR operator same as cpp;
+ let name="kuldeep",age=34;
+ if(name[2]=== 'l' || age>=22){
+    console.log("adult");
+ }
+ else{
+    Minor;
+}
+ let sum=0,i=1;
+
+ while(i<=10){
+    sum+=i;
+    i++;
+ }
+  console.log(sum);
+
+var sum=0;
+ for(let i=1;i<9;i++){
+    sum+=i;
+ }
+ console.log("sum is :",sum);
+
+ var sum=0,i=1;
+do{
+    sum+=i;
+    i++;
+}while(i<=9);
+console.log(sum);
+
+ye saare same c++ ki trah hi hai isse upper ka
+*/
